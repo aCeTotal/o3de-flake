533799a6acec5f58c0367c77c30c304f1fdca494
aCeTotal <lars.oksendal@gmail.com>
2025-03-29 20:28:49 +0100

    pyproject

--- flake.nix
+++ flake.nix
@@ -1,128 +1,103 @@
 {
-  description = "O3DE Game Engine flake with customizable source";
+    description = "O3DE Game Engine flake with customizable source";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    fork = {
-    url = "github:o3de/o3de";
-    flake = false;
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
+        flake-utils.url = "github:numtide/flake-utils";
+        fork = {
+            url = "github:o3de/o3de";
+            flake = false;
+        };
     };
 
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { self, nixpkgs, flake-utils, fork, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-
-        o3dePackages = import ./o3de-packages/python.nix { inherit pkgs; };
-        o3de-src = fork;
-
-        cmakeFlags = [
-          "-DLY_3RDPARTY_PATH=${pkgs.stdenv.mkDerivation {
-            name = "o3de-3rdparty";
-            src = null;
-            installPhase = "mkdir -p $out";
-          }}"
-        ];
-      in {
-        packages.default = pkgs.stdenv.mkDerivation {
-          pname = "o3de";
-          version = "dev";
-          src = o3de-src;
-
-          nativeBuildInputs = [
-            o3dePackages.o3dePython
-            pkgs.cmake
-            pkgs.ninja
-            pkgs.git
-            pkgs.pkg-config
-          ];
-
-          buildInputs = [
-            o3dePackages.o3dePython
-
-            pkgs.clang_16
-            pkgs.libstdcxx-12
-
-            pkgs.vulkan-loader
-            pkgs.vulkan-headers
-            pkgs.mesa
-            pkgs.libGL
-            pkgs.libunwind
-            pkgs.zlib
-            pkgs.zstd
-
-            pkgs.xorg.libX11
-            pkgs.xorg.libXcursor
-            pkgs.xorg.libXi
-            pkgs.xorg.libXrandr
-            pkgs.xorg.libXxf86vm
-            pkgs.xorg.libXinerama
-            pkgs.xorg.libXrender
-
-            pkgs.libxkbcommon
-            pkgs.libxkbcommon.dev
-            pkgs.libxkbcommon-x11
-            pkgs.libxkbcommon-x11.dev
-
-            pkgs.xcbutil
-            pkgs.xcbutilkeysyms
-            pkgs.xcbutilwm
-            pkgs.xcbutilimage
-            pkgs.xcbutilcursor
-
-            pkgs.xorg.libxcb
-            pkgs.xorg.libxcb.dev
-            pkgs.xorg.libxcb.lib
-            pkgs.xorg.libxcb.out
-
-            pkgs.xorg.libxcbRandr
-            pkgs.xorg.libxcbXinerama
-            pkgs.xorg.libxcbXinput
-            pkgs.xorg.libxcbXfixes
-
-            pkgs.fontconfig
-            pkgs.pcre2
-
-            pkgs.tcl
-            pkgs.tk
-            pkgs.tclPackages.tix
-          ];
-
-          configurePhase = ''
+    outputs = { self, nixpkgs, pyproject-nix, flake-utils, fork, ... }:
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = import nixpkgs { inherit system; };
+                project = pyproject-nix.lib.project.loadRequirementsTxt {
+                    requirements = builtins.readFile ./o3de-packages/requirements.txt;
+                };
+                python = pkgs.python311;
+                pythonEnv = python.withPackages (project.renderers.withPackages { inherit python; });
+            in {
+                packages.default = pkgs.stdenv.mkDerivation {
+                    pname = "o3de";
+                    version = "dev";
+                    src = fork;
+                    nativeBuildInputs = [
+                        pythonEnv
+                        pkgs.cmake
+                        pkgs.ninja
+                        pkgs.git
+                        pkgs.pkg-config
+                    ];
+                    buildInputs = [
+                        pythonEnv
+                        pkgs.clang_16
+                        pkgs.libstdcxx-12
+                        pkgs.vulkan-loader
+                        pkgs.vulkan-headers
+                        pkgs.mesa
+                        pkgs.libGL
+                        pkgs.libunwind
+                        pkgs.zlib
+                        pkgs.zstd
+                        pkgs.xorg.libX11
+                        pkgs.xorg.libXcursor
+                        pkgs.xorg.libXi
+                        pkgs.xorg.libXrandr
+                        pkgs.xorg.libXxf86vm
+                        pkgs.xorg.libXinerama
+                        pkgs.xorg.libXrender
+                        pkgs.libxkbcommon
+                        pkgs.libxkbcommon.dev
+                        pkgs.libxkbcommon-x11
+                        pkgs.libxkbcommon-x11.dev
+                        pkgs.xcbutil
+                        pkgs.xcbutilkeysyms
+                        pkgs.xcbutilwm
+                        pkgs.xcbutilimage
+                        pkgs.xcbutilcursor
+                        pkgs.xorg.libxcb
+                        pkgs.xorg.libxcb.dev
+                        pkgs.xorg.libxcb.lib
+                        pkgs.xorg.libxcb.out
+                        pkgs.xorg.libxcbRandr
+                        pkgs.xorg.libxcbXinerama
+                        pkgs.xorg.libxcbXinput
+                        pkgs.xorg.libxcbXfixes
+                        pkgs.fontconfig
+                        pkgs.pcre2
+                        pkgs.tcl
+                        pkgs.tk
+                        pkgs.tclPackages.tix
+                    ];
+                    configurePhase = ''
             echo "✅ Python versjon: $(python3 --version)"
             cmake -B build -S . \
-              -GNinja \
-              ${builtins.concatStringsSep " \\\n" cmakeFlags}
-          '';
-
-          buildPhase = "ninja -C build";
-
-          installPhase = ''
+            -GNinja
+            '';
+                    buildPhase = "ninja -C build";
+                    installPhase = ''
             mkdir -p $out
             cp -r build/bin $out/
-          '';
-        };
+            '';
+                };
 
-        devShells.default = pkgs.mkShell {
-          packages = [
-            o3dePackages.o3dePython
-            pkgs.cmake
-            pkgs.ninja
-            pkgs.clang_16
-            pkgs.git
-          ];
-
-          shellHook = ''
+                devShells.default = pkgs.mkShell {
+                    packages = [
+                        pythonEnv
+                        pkgs.cmake
+                        pkgs.ninja
+                        pkgs.clang_16
+                        pkgs.git
+                    ];
+                    shellHook = ''
             echo "O3DE devShell"
             echo "Python: $(which python3)"
-          '';
-        };
-      });
+            '';
+                };
+            });
 }
 
--- o3de-packages/python.nix
+++ o3de-packages/python.nix
@@ -1,27 +1,17 @@
-{ pkgs }:
+{ pkgs ? import <nixpkgs> { } }:
 
 let
-  requirements = import ./requirements.nix;
+  mach-nix = import (builtins.fetchTarball {
+    url = "https://github.com/DavHau/mach-nix/archive/refs/tags/3.5.0.tar.gz";
+    sha256 = "sha256:185qf6d5xg8qk1hb1y0b5gggr71vdz8v9d5ga4zg7dmcb1aypxcg";
+  }) {
+    inherit pkgs;
+  };
 
-  inherit (pkgs) fetchPypi python3;
-
-  # Vi bygger hver pakke fra PyPI og pins
-  builtPackages = map (pkg:
-    python3.pkgs.buildPythonPackage {
-      pname = pkg.pname;
-      version = pkg.version;
-      format = "setuptools";
-
-      src = fetchPypi {
-        inherit (pkg) pname version sha256;
-      };
-
-      doCheck = false;
-    }
-  ) requirements;
-
-  # Sett sammen alle pakkene i én Python-closure
-  pythonEnv = python3.withPackages (_: builtPackages);
+  pythonEnv = mach-nix.mkPython {
+    python = "python311";  # <-- må være string
+    requirements = builtins.readFile ./requirements.txt;
+  };
 
 in {
   o3dePython = pythonEnv;
